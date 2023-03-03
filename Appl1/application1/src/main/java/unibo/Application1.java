d42485ace31d4860628fcbac2ef68bb8c2b94f82
Antonio Natali <antonio.natali@unibo.it>
2023-03-03 12:47:23 +0100

    Appl1

---
+++ Appl1/application1/src/main/java/unibo/Application1.java
@@ -0,0 +1,8 @@
+package unibo;
+import unibo.basicomm23.utils.CommUtils;
+public class Application1 {
+    public static void main( String[] args ){
+        System.out.println("Application1 hello");
+        CommUtils.outblue("Application1 hello again");
+    }
+}

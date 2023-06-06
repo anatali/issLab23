1c512d2bfc56ddc8b81eaf5e50207424142d1c1c
Antonio Natali <antonio.natali@unibo.it>
2023-06-06 12:05:59 +0200

    ptemafinale

--- unibo.sonarqak23/sonarqak23.pl
+++ unibo.sonarqak23/sonarqak23.pl
@@ -2,11 +2,11 @@
 % sonarqak23 description   
 %====================================================================================
 context(ctxsonarqak23, "localhost",  "TCP", "8128").
-context(ctxbasicrobot, "192.168.1.9",  "TCP", "8020").
- qactor( basicrobot, ctxbasicrobot, "external").
+%%context(ctxbasicrobot, "192.168.1.9",  "TCP", "8020").
+%% qactor( basicrobot, ctxbasicrobot, "external").
   qactor( sonar, ctxsonarqak23, "sonarSimulator").
   qactor( datacleaner, ctxsonarqak23, "rx.dataCleaner").
   qactor( distancefilter, ctxsonarqak23, "rx.distanceFilter").
   qactor( sonar23, ctxsonarqak23, "it.unibo.sonar23.Sonar23").
   qactor( sonar23observer, ctxsonarqak23, "it.unibo.sonar23observer.Sonar23observer").
-  qactor( appl, ctxsonarqak23, "it.unibo.appl.Appl").
+%%  qactor( appl, ctxsonarqak23, "it.unibo.appl.Appl").
